9c63600721d5039effc08ef419d50f8a5181830a
Shriom <shriomtripathi33@gmail.com>
2019-05-19 21:13:47 +0530

    dependency setup and SplashActivty

---
+++ app/src/main/java/com/example/joker/sistofoodtest/SplashActivity.java
@@ -0,0 +1,84 @@
+package com.example.joker.sistofoodtest;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.joker.sistofoodtest.helper.FirebaseHelper;
+import com.example.joker.sistofoodtest.helper.SharedPref;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 100;
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        FirebaseHelper.init(getApplicationContext());
+
+        ImageView imageView = findViewById(R.id.logoImageView);
+        Glide.with(this).load(R.drawable.instagram_log).into(imageView);
+
+        final FirebaseAuth auth = FirebaseHelper.getFirebaseAuth();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (auth.getCurrentUser() == null) {
+                    // Create and launch sign-in intent
+                    startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
+
+                } else {
+                    gotoMainActivity();
+                }
+            }
+        }, 1500);
+
+    }
+
+    private void gotoMainActivity() {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                SharedPref.getInstance(getApplicationContext()).saveUser(user);
+                FirebaseHelper.saveUser(getApplicationContext());
+                gotoMainActivity();
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                Toast.makeText(SplashActivity.this, "Authentication Error", Toast.LENGTH_SHORT).show();
+                Log.d("auth_error", response.getError().toString());
+
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/joker/sistofoodtest/helper/FirebaseHelper.java
@@ -0,0 +1,101 @@
+package com.example.joker.sistofoodtest.helper;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.example.joker.sistofoodtest.Models.User;
+
+import java.io.File;
+import java.util.Date;
+
+
+public class FirebaseHelper {
+
+    private static FirebaseAuth auth;
+    private static FirebaseDatabase database;
+    private static FirebaseStorage storage;
+
+    private static DatabaseReference userReference, postReference;
+
+    private static StorageReference storageReference, imageReference;
+
+    public static void init(Context context) {
+
+
+        //FirebaseApp.initializeApp(context);
+
+        //getInstance
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        //created database reference
+        userReference = database.getReference("users");
+        postReference = database.getReference("posts");
+
+        storageReference = storage.getReference();
+
+    }
+
+    public static UploadTask setFile(String imageLocation) {
+
+        Uri file = Uri.fromFile(new File(imageLocation));
+        imageReference = storageReference.child(file.getLastPathSegment());
+        return imageReference.putFile(file);
+
+    }
+
+    public static StorageReference getImageRef() {
+        return imageReference;
+    }
+
+    public static DatabaseReference getPostRef() {
+        return postReference;
+    }
+
+    public static FirebaseAuth getFirebaseAuth() {
+        return auth;
+    }
+
+    public static boolean isLoggedIn() {
+        return auth.getCurrentUser() != null;
+    }
+
+    public static void saveUser(Context context) {
+        User user = SharedPref.getInstance(context).getUser();
+        userReference.child(user.getUserId()).setValue(user);
+    }
+
+    public static String getTime(long seconds) {
+        long timeMilli = new Date().getTime();
+        long currentSecond = timeMilli / 1000;
+        seconds = currentSecond - seconds;
+        if (seconds > 59) {
+            long minutes = seconds / 60;
+            if (minutes > 59) {
+                long hours = minutes / 60;
+                if (hours > 23) {
+                    int days = (int) hours / 24;
+                    if (days > 364) {
+                        int year = days / 365;
+                        return String.valueOf(year) + " years ";
+                    } else {
+                        return String.valueOf(days) + " days ago";
+                    }
+                } else {
+                    return String.valueOf(hours) + " hours ago";
+                }
+            } else {
+                return String.valueOf(minutes) + " mins ago";
+            }
+        } else {
+            return String.valueOf(seconds) + " seconds ago";
+        }
+    }
+}
